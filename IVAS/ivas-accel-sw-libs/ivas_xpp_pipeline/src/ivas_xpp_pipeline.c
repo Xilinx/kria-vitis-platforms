349f4e5743863d27c92d63788180112827051d67
yuxiz <yuxiz@xilinx.com>
2020-12-10 20:40:56 +0800

    IVAS pp plugin update for stride support

--- IVAS/ivas-accel-sw-libs/ivas_xpp_pipeline/src/ivas_xpp_pipeline.c
+++ IVAS/ivas-accel-sw-libs/ivas_xpp_pipeline/src/ivas_xpp_pipeline.c
@@ -14,13 +14,9 @@
  * limitations under the License.
  */
 
+#include <ivas/ivas_kernel.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <ivas/ivaslogs.h>
-#include <ivas/ivas_kernel.h>
-
-#define DEFAULT_PPC	2
 
 typedef struct _kern_priv
 {
@@ -30,13 +26,11 @@ typedef struct _kern_priv
     float scale_r;
     float scale_g;
     float scale_b;
-    int bias; /* for future */
-    int ppc;
-    IVASFrame *mem;
-    int log_level;
+    IVASFrame *params;
 } ResizeKernelPriv;
 
-int32_t xlnx_kernel_start(IVASKernel *handle, int start, IVASFrame *input[MAX_NUM_OBJECT], IVASFrame *output[MAX_NUM_OBJECT]);
+int32_t
+xlnx_kernel_start(IVASKernel *handle, int start, IVASFrame *input[MAX_NUM_OBJECT], IVASFrame *output[MAX_NUM_OBJECT]);
 int32_t xlnx_kernel_done(IVASKernel *handle);
 int32_t xlnx_kernel_init(IVASKernel *handle);
 uint32_t xlnx_kernel_deinit(IVASKernel *handle);
@@ -45,7 +39,7 @@ uint32_t xlnx_kernel_deinit(IVASKernel *handle)
 {
     ResizeKernelPriv *kernel_priv;
     kernel_priv = (ResizeKernelPriv *)handle->kernel_priv;
-    ivas_free_buffer (handle, kernel_priv->mem);
+    ivas_free_buffer (handle, kernel_priv->params);
     free(kernel_priv);
     return 0;
 }
@@ -55,37 +49,21 @@ int32_t xlnx_kernel_init(IVASKernel *handle)
     json_t *jconfig = handle->kernel_config;
     json_t *val; /* kernel config from app */
     ResizeKernelPriv *kernel_priv;
-    float *pPtr;
-
-    handle->is_multiprocess = 0;
+    float *pPtr; 
 
     kernel_priv = (ResizeKernelPriv *)calloc(1, sizeof(ResizeKernelPriv));
     if (!kernel_priv) {
-        printf("Error: Unable to allocate PPE kernel memory\n");
+        printf("Error: Unable to allocate resize kernel memory\n");
     }
 
     /* parse config */
-    val = json_object_get (jconfig, "debug_level");
-    if (!val || !json_is_integer (val))
-	    kernel_priv->log_level = LOG_LEVEL_WARNING;
-    else
-	    kernel_priv->log_level = json_integer_value (val);
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: debug_level %d", kernel_priv->log_level);
-
-    val = json_object_get (jconfig, "ppc");
-    if (!val || !json_is_integer (val))
-	    kernel_priv->ppc = DEFAULT_PPC;
-    else
-	    kernel_priv->ppc = json_integer_value (val);
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: PPC %d", kernel_priv->ppc);
-
     val = json_object_get(jconfig, "mean_r");
     if (!val || !json_is_number(val))
         kernel_priv->mean_r = 0;
     else {
         kernel_priv->mean_r = json_number_value(val);
     }
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: mean R = %lf", kernel_priv->mean_r);
+    printf("Resize: mean_r=%f\n", kernel_priv->mean_r);
 
     val = json_object_get(jconfig, "mean_g");
     if (!val || !json_is_number(val))
@@ -93,7 +71,7 @@ int32_t xlnx_kernel_init(IVASKernel *handle)
     else {
         kernel_priv->mean_g = json_number_value(val);
     }
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: mean G = %lf", kernel_priv->mean_g);
+    printf("Resize: mean_g=%f\n", kernel_priv->mean_g);
 
     val = json_object_get(jconfig, "mean_b");
     if (!val || !json_is_number(val))
@@ -101,46 +79,38 @@ int32_t xlnx_kernel_init(IVASKernel *handle)
     else {
         kernel_priv->mean_b = json_number_value(val);
     }
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: mean B = %lf", kernel_priv->mean_b);
+    printf("Resize: mean_b=%f\n", kernel_priv->mean_b);
 
+    /* parse config */
     val = json_object_get(jconfig, "scale_r");
     if (!val || !json_is_number(val))
 	kernel_priv->scale_r = 1;
     else
 	kernel_priv->scale_r = json_number_value(val);
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: Scale R value %lf", kernel_priv->scale_r);
+    printf("Resize: scale_r=%f\n", kernel_priv->scale_r);
 
     val = json_object_get(jconfig, "scale_g");
     if (!val || !json_is_number(val))
 	kernel_priv->scale_g = 1;
     else
 	kernel_priv->scale_g = json_number_value(val);
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: Scale G value %lf", kernel_priv->scale_g);
+    printf("Resize: scale_g=%f\n", kernel_priv->scale_g);
 
     val = json_object_get(jconfig, "scale_b");
     if (!val || !json_is_number(val))
 	kernel_priv->scale_b = 1;
     else
 	kernel_priv->scale_b = json_number_value(val);
-    LOG_MESSAGE (LOG_LEVEL_INFO, kernel_priv->log_level, "IVAS PPE: Scale B value %lf", kernel_priv->scale_b);
+    printf("Resize: scale_b=%f\n", kernel_priv->scale_b);
 
-    kernel_priv->mem = ivas_alloc_buffer (handle, 9*(sizeof(float)), IVAS_INTERNAL_MEMORY, NULL);
-    if (!kernel_priv->mem) {
-    	    LOG_MESSAGE (LOG_LEVEL_ERROR, kernel_priv->log_level, "IVAS PPE: failed to allocate descriptor memory");
-	    return -1;
-    }
-
-    pPtr = (float *)kernel_priv->mem->vaddr[0];
-
-    pPtr[0] = (float)kernel_priv->mean_r;
-    pPtr[1] = (float)kernel_priv->mean_g;
-    pPtr[2] = (float)kernel_priv->mean_b;
-    pPtr[3] = (float)kernel_priv->scale_r;
-    pPtr[4] = (float)kernel_priv->scale_g;
-    pPtr[5] = (float)kernel_priv->scale_b;
-    pPtr[6] = (float)0;
-    pPtr[7] = (float)0;
-    pPtr[8] = (float)0;
+    kernel_priv->params = ivas_alloc_buffer (handle, 6*(sizeof(float)), IVAS_INTERNAL_MEMORY, NULL);
+    pPtr = kernel_priv->params->vaddr[0];
+    pPtr[0] = (float)kernel_priv->mean_r;  
+    pPtr[1] = (float)kernel_priv->mean_g;  
+    pPtr[2] = (float)kernel_priv->mean_b;  
+    pPtr[3] = (float)kernel_priv->scale_r;  
+    pPtr[4] = (float)kernel_priv->scale_g;  
+    pPtr[5] = (float)kernel_priv->scale_b;  
 
     handle->kernel_priv = (void *)kernel_priv;
 
@@ -149,42 +119,21 @@ int32_t xlnx_kernel_init(IVASKernel *handle)
 
 int32_t xlnx_kernel_start(IVASKernel *handle, int start, IVASFrame *input[MAX_NUM_OBJECT], IVASFrame *output[MAX_NUM_OBJECT])
 {
-    int uv_h, uv_w;
-    uint64_t coeff_phy;
-    int th1, th2;
     ResizeKernelPriv *kernel_priv;
-    uv_h = (input[0]->props.height)/2;
-    uv_w = (input[0]->props.width)/2;
-
-    th1 = 255;
-    th2 = 255;
-
     kernel_priv = (ResizeKernelPriv *)handle->kernel_priv;
-    coeff_phy = kernel_priv->mem->paddr[0];
 
-//    if (output[0]->props.width % kernel_priv->ppc != 0) {
-//	    LOG_MESSAGE (LOG_LEVEL_ERROR, kernel_priv->log_level, "IVAS PPE: Out width not align to ppc !!");
-//	    return -1;
-//    }
+    ivas_register_write(handle, &(input[0]->props.width), sizeof(uint32_t), 0x40);   /* In width */
+    ivas_register_write(handle, &(input[0]->props.height), sizeof(uint32_t), 0x48);  /* In height */
+    ivas_register_write(handle, &(input[0]->props.stride), sizeof(uint32_t), 0x50);  /* In stride */
 
-    ivas_register_write(handle, &(input[0]->props.height), sizeof(uint32_t), 0x34);  /* In Y height */
-    ivas_register_write(handle, &(input[0]->props.width), sizeof(uint32_t), 0x3C);   /* In Y width */
+    ivas_register_write(handle, &(output[0]->props.width), sizeof(uint32_t), 0x58);  /* Out width */
+    ivas_register_write(handle, &(output[0]->props.height), sizeof(uint32_t), 0x60); /* Out height */
+    ivas_register_write(handle, &(output[0]->props.width), sizeof(uint32_t), 0x68); /* Out stride */
 
-    ivas_register_write(handle, &(uv_h), sizeof(uint32_t), 0x44);  /* In UV height */
-    ivas_register_write(handle, &(uv_w), sizeof(uint32_t), 0x4C);  /* In UV width */
-
-    ivas_register_write(handle, &(output[0]->props.height), sizeof(uint32_t), 0x54); /* Out height */
-    ivas_register_write(handle, &(output[0]->props.width), sizeof(uint32_t), 0x5C);  /* Out width */
-
-    ivas_register_write(handle, &(input[0]->paddr[0]), sizeof(uint64_t), 0x10);      /* Y input */
-    ivas_register_write(handle, &(input[0]->paddr[1]), sizeof(uint64_t), 0x1C);      /* UV input */
-    ivas_register_write(handle, &(output[0]->paddr[0]), sizeof(uint64_t), 0x28);     /* Output buffer */
-
-//    ivas_register_write(handle, &(input[0]->props.stride), sizeof(uint32_t), 0x64); /* stride */
-    ivas_register_write(handle, &(coeff_phy), sizeof(uint64_t), 0x64); /* coeff mem phy add */
-
-    ivas_register_write(handle, &(th1), sizeof(uint32_t), 0x70);
-    ivas_register_write(handle, &(th2), sizeof(uint32_t), 0x78);
+    ivas_register_write(handle, &(input[0]->paddr[0]), sizeof(uint64_t), 0x10);      /* Y Input */
+    ivas_register_write(handle, &(input[0]->paddr[1]), sizeof(uint64_t), 0x1C);      /* UV Input */
+    ivas_register_write(handle, &(output[0]->paddr[0]), sizeof(uint64_t), 0x28);      /* Output */
+    ivas_register_write(handle, &(kernel_priv->params->paddr[0]), sizeof(uint64_t), 0x34);     /* Params */
 
     ivas_register_write(handle, &start, sizeof(uint32_t), 0x0);                      /* start */
     return 0;
@@ -193,14 +142,11 @@ int32_t xlnx_kernel_start(IVASKernel *handle, int start, IVASFrame *input[MAX_NU
 int32_t xlnx_kernel_done(IVASKernel *handle)
 {
     uint32_t val = 0, count = 0;
-    ResizeKernelPriv *kernel_priv;
-
-    kernel_priv = (ResizeKernelPriv *)handle->kernel_priv;
     do {
         ivas_register_read(handle, &val, sizeof(uint32_t), 0x0); /* start */
         count++;
         if (count > 1000000) {
-    	    LOG_MESSAGE (LOG_LEVEL_ERROR, kernel_priv->log_level, "IVAS PPE: kernel done wait TIME OUT !!");
+            printf("ERROR: kernel done wait TIME OUT !!\n");
             return 0;
         }
     } while (!(0x4 & val));
