f184a2a035782ac5cff070f9912e93f03632e30a
jiaz <jiaz@xilinx.com>
2022-09-05 17:41:34 +0800

    update aibox

--- kv260/overlays/examples/aibox-reid/xf_pp_pipeline_accel.cpp
+++ kv260/overlays/examples/aibox-reid/xf_pp_pipeline_accel.cpp
@@ -37,37 +37,36 @@ void pp_pipeline_accel(ap_uint<INPUT_PTR_WIDTH>* img_inp_y,  // Y Input image po
 #pragma HLS INTERFACE s_axilite port=out_img_linestride     
 #pragma HLS INTERFACE s_axilite port=return
     // clang-format on
-    xf::cv::Mat<XF_8UC1, HEIGHT, WIDTH, NPC> imgInput_y(in_img_height, in_img_width);
+    xf::cv::Mat<XF_8UC1, HEIGHT, WIDTH, NPC,XF_CV_DEPTH> imgInput_y(in_img_height, in_img_width);
 #pragma HLS stream variable = imgInput_y.data depth = 2
-    xf::cv::Mat<XF_8UC2, HEIGHT/2, WIDTH/2, NPC> imgInput_uv(in_img_height/2, in_img_width/2);	
+    xf::cv::Mat<XF_8UC2, HEIGHT/2, WIDTH/2, NPC,XF_CV_DEPTH> imgInput_uv(in_img_height/2, in_img_width/2);	
 #pragma HLS stream variable = imgInput_uv.data depth = 2
-    xf::cv::Mat<XF_8UC3, HEIGHT, WIDTH, NPC> rgb_mat(in_img_height, in_img_width);
+    xf::cv::Mat<XF_8UC3, HEIGHT, WIDTH, NPC,XF_CV_DEPTH> rgb_mat(in_img_height, in_img_width);
 #pragma HLS stream variable = rgb_mat.data depth = 2
  
 #if BGR2RGB
-    xf::cv::Mat<XF_8UC3, HEIGHT, WIDTH, NPC> ch_swap_mat(in_img_height, in_img_width);
+    xf::cv::Mat<XF_8UC3, HEIGHT, WIDTH, NPC,XF_CV_DEPTH> ch_swap_mat(in_img_height, in_img_width);
 #endif
-    xf::cv::Mat<XF_8UC3, NEWHEIGHT, NEWWIDTH, NPC> resize_out_mat(out_img_height, out_img_width);
+    xf::cv::Mat<XF_8UC3, NEWHEIGHT, NEWWIDTH, NPC,XF_CV_DEPTH> resize_out_mat(out_img_height, out_img_width);
 #pragma HLS stream variable = resize_out_mat.data depth = 2
-    xf::cv::Mat<OUT_TYPE, NEWHEIGHT, NEWWIDTH, NPC> out_mat(out_img_height, out_img_width);
+    xf::cv::Mat<OUT_TYPE, NEWHEIGHT, NEWWIDTH, NPC,XF_CV_DEPTH> out_mat(out_img_height, out_img_width);
 // clang-format off
 #pragma HLS stream variable = out_mat.data depth = 2
     xf::cv::accel_utils obj_iny, obj_inuv;
 #pragma HLS DATAFLOW
     // clang-format on
-    obj_iny.Array2xfMat<INPUT_PTR_WIDTH, XF_8UC1, HEIGHT, WIDTH, NPC>(img_inp_y, imgInput_y, in_img_linestride);
-    obj_inuv.Array2xfMat<INPUT_PTR_WIDTH, XF_8UC2, HEIGHT/2, WIDTH/2, NPC> (img_inp_uv, imgInput_uv, in_img_linestride/2);
-    xf::cv::nv122bgr<XF_8UC1, XF_8UC2, XF_8UC3, HEIGHT, WIDTH, NPC, NPC>(imgInput_y, imgInput_uv, rgb_mat);
+    obj_iny.Array2xfMat<INPUT_PTR_WIDTH, XF_8UC1, HEIGHT, WIDTH, NPC,XF_CV_DEPTH>(img_inp_y, imgInput_y, in_img_linestride);
+    obj_inuv.Array2xfMat<INPUT_PTR_WIDTH, XF_8UC2, HEIGHT/2, WIDTH/2, NPC,XF_CV_DEPTH> (img_inp_uv, imgInput_uv, in_img_linestride/2);
+    xf::cv::nv122bgr<XF_8UC1, XF_8UC2, XF_8UC3, HEIGHT, WIDTH, NPC, NPC,XF_CV_DEPTH,XF_CV_DEPTH,XF_CV_DEPTH>(imgInput_y, imgInput_uv, rgb_mat);
  
 #if BGR2RGB
-    xf::cv::bgr2rgb<IN_TYPE, OUT_TYPE, HEIGHT, WIDTH, NPC>(rgb_mat, ch_swap_mat);
-    xf::cv::resize<INTERPOLATION, IN_TYPE, HEIGHT, WIDTH, NEWHEIGHT, NEWWIDTH, NPC, MAXDOWNSCALE>(ch_swap_mat,
+    xf::cv::bgr2rgb<IN_TYPE, OUT_TYPE, HEIGHT, WIDTH, NPC,XF_CV_DEPTH,XF_CV_DEPTH>(rgb_mat, ch_swap_mat);
+    xf::cv::resize<INTERPOLATION, IN_TYPE, HEIGHT, WIDTH, NEWHEIGHT, NEWWIDTH, NPC, MAXDOWNSCALE,XF_CV_DEPTH,XF_CV_DEPTH>(ch_swap_mat,
                                                                                                   resize_out_mat);
 #else
-    xf::cv::resize<INTERPOLATION, IN_TYPE, HEIGHT, WIDTH, NEWHEIGHT, NEWWIDTH, NPC, MAXDOWNSCALE>(rgb_mat,
+    xf::cv::resize<INTERPOLATION, IN_TYPE, HEIGHT, WIDTH, NEWHEIGHT, NEWWIDTH, NPC,MAXDOWNSCALE,XF_CV_DEPTH,XF_CV_DEPTH>(rgb_mat,
                                                                                                   resize_out_mat);
 #endif
-    xf::cv::preProcess<IN_TYPE, OUT_TYPE, NEWHEIGHT, NEWWIDTH, NPC, WIDTH_A, IBITS_A, WIDTH_B, IBITS_B, WIDTH_OUT,
-                       IBITS_OUT>(resize_out_mat, out_mat, params);
-    xf::cv::xfMat2Array<OUTPUT_PTR_WIDTH, OUT_TYPE, NEWHEIGHT, NEWWIDTH, NPC>(out_mat, img_out, out_img_linestride);
+    xf::cv::preProcess<IN_TYPE, OUT_TYPE, NEWHEIGHT, NEWWIDTH, NPC,WIDTH_A, IBITS_A, WIDTH_B, IBITS_B, WIDTH_OUT,IBITS_OUT,XF_CV_DEPTH,XF_CV_DEPTH>(resize_out_mat, out_mat, params);
+    xf::cv::xfMat2Array<OUTPUT_PTR_WIDTH, OUT_TYPE, NEWHEIGHT, NEWWIDTH, NPC,XF_CV_DEPTH>(out_mat, img_out, out_img_linestride);
 }
